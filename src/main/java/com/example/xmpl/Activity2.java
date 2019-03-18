67bc85ae670b45128ffac6e2852426272d474a4a
Peretz54 <gil_1998@mail.ru>
2019-03-18 19:04:53 +0300

    Second

--- src/main/java/com/example/xmpl/Activity2.java
+++ src/main/java/com/example/xmpl/Activity2.java
@@ -1,37 +1,49 @@
 package com.example.xmpl;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "message";
+
+    public static void start(Activity activity, String message) {
+        Intent intent = new Intent(activity, Activity2.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        activity.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        String Str = getIntent().getStringExtra("message");
-        TextView infoTextView = (TextView)findViewById(R.id.textView);
-        infoTextView.setText(Str);
-    }
-    public void email(View view) {
-        TextView userTextView = (TextView) findViewById(R.id.textView);
+        Intent intent = getIntent();
+        TextView infoTextView = findViewById(R.id.textView);
+        infoTextView.setText(intent.getStringExtra(EXTRA_MESSAGE));
 
+            Button emailButton = findViewById(R.id.button);
+            emailButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    email(v);
+                }
+            });
+    }
+
+    public void email(View view) {
+        TextView userTextView = findViewById(R.id.textView);
         Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
 
         emailIntent.setData(Uri.parse("mailto:"));
-     //   emailIntent.setType("text/plain");
-
- //       emailIntent.setDataAndType(Uri.parse("mailto:"), "text/plain");
         emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, userTextView.getText().toString());
-        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,"address");
-        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,"topic");
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, "address");
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "topic");
 
         if (emailIntent.resolveActivity(getPackageManager()) != null) {
             Toast.makeText(Activity2.this, "Opening mailing app", Toast.LENGTH_SHORT).show();
--- src/main/java/com/example/xmpl/MainActivity.java
+++ src/main/java/com/example/xmpl/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.xmpl;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button previewButton = findViewById(R.id.bPreview);
+        previewButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText userEditText = findViewById(R.id.editText);
+                Activity2.start(MainActivity.this, userEditText.getText().toString());
+            }
+        });
     }
 
-    public void onClick(View view) {
-        EditText userEditText = (EditText) findViewById(R.id.editText);
-        Intent intent = new Intent(MainActivity.this, Activity2.class);
-        intent.putExtra("message", userEditText.getText().toString());
-        startActivity(intent);
-    }
+
+//    public void onClick(View view) {
+//        EditText userEditText = (EditText) findViewById(R.id.editText);
+//        Intent intent = new Intent(MainActivity.this, Activity2.class);
+//        intent.putExtra("message", userEditText.getText().toString());
+//        startActivity(intent);
+//    }
 }
